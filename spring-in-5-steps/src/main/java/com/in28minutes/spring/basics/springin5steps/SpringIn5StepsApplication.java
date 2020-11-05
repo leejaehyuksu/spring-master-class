f2b116ec919c886dd0cda74c35b8b2f44df81458
leejaehyuksu <a01050973508@gmail.com>
2020-11-05 10:51:47 +0900

    2020-11-05 studt commit

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -2,16 +2,20 @@ package com.in28minutes.spring.basics.springin5steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringIn5StepsApplication {
 
 	public static void main(String[] args) {
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
-		int result = binarySearch.binarySearch(new int[] {12, 4, 6}, 3);
-		System.out.println(result);
-		
-		//SpringApplication.run(SpringIn5StepsApplication.class, args);
+		//BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+		//System.out.println(result);
+		ApplicationContext applicationContext = 
+				SpringApplication.run(SpringIn5StepsApplication.class, args);
+		BinarySearchImpl binarySearch = 
+				applicationContext.getBean(BinarySearchImpl.class);
+		int result = 
+				binarySearch.binarySearch(new int[] {12, 4, 6}, 3);
+		System.out.println(result);	
 	}
-
 }
